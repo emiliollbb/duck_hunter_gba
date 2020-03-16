7729b026ae6e40d237a1d141c4534a20adf08c51
Emilio Lopez Berenguer <emiliollbb@gmail.com>
2020-03-16 15:05:42 +0100

    Initial

--- background.h
+++ background.h
@@ -5,11 +5,11 @@
 //
 //	background, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 23 tiles (t|f|p reduced) not compressed
+//	+ 6 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 736 + 2048 = 3296
+//	Total size: 512 + 192 + 2048 = 2752
 //
-//	Time-stamp: 2020-03-16, 14:31:42
+//	Time-stamp: 2020-03-16, 15:04:26
 //	Exported by Cearn's Usenti v1.7.7
 //	(comments, kudos, flames to "daytshen@hotmail.com")
 //
@@ -21,8 +21,8 @@
 #define backgroundPalLen 512
 extern const unsigned short backgroundPal[256];
 
-#define backgroundTilesLen 736
-extern const unsigned short backgroundTiles[368];
+#define backgroundTilesLen 192
+extern const unsigned short backgroundTiles[96];
 
 #define backgroundMapLen 2048
 extern const unsigned short backgroundMap[1024];
--- duck_hunter.c
+++ duck_hunter.c
@@ -23,6 +23,9 @@ int main()
 	//   using charblock 0 and screenblock 31
 	REG_BG0CNT= BG_CBB(0) | BG_SBB(30) | BG_4BPP | BG_REG_64x32;
 	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+	
+	REG_BG0HOFS= 0;
+	REG_BG0VOFS= 0;
 
 	// Scroll around some
 	int x= 192, y= 64;
@@ -34,8 +37,7 @@ int main()
 		x += key_tri_horz();
 		y += key_tri_vert();
 
-		REG_BG0HOFS= x;
-		REG_BG0VOFS= y;
+		
 	}
 
 	return 0;
